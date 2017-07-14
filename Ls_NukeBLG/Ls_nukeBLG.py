ae2ce438f9ab364b604575daa6bd00816da2eda4
Lewis Saunders <lewis.saunders@gmail.com>
2017-07-14 18:05:29 +0100

    nukeBLG name change

--- Ls_NukeBLG/Ls_nukeBLG.py
+++ Ls_NukeBLG/Ls_nukeBLG.py
@@ -5,15 +5,15 @@
 
 import os, sys
 nukebin = '/Applications/Nuke10.5v4/Nuke10.5v4.app/Contents/MacOS/Nuke10.5v4'
-frontreadyfifo = '/tmp/nukeBLGfrontready'
-resultreadyfifo = '/tmp/nukeBLGresultready'
-blg = '/tmp/nukeBLG.blg.exr'
+frontreadyfifo = '/tmp/Ls_nukeBLGfrontready'
+resultreadyfifo = '/tmp/Ls_nukeBLGresultready'
+blg = '/tmp/Ls_nukeBLG.blg.exr'
 
 if('nuke' in sys.modules):
 	# We're being run from the Nuke process - set up the tree
-	r = nuke.createNode('Read', 'file /tmp/nukeBLGfront.exr', False)
+	r = nuke.createNode('Read', 'file /tmp/Ls_nukeBLGfront.exr', False)
 	b = nuke.createNode('Baselight', '', False)
-	w = nuke.createNode('Write', 'file /tmp/nukeBLGresult.exr', False)
+	w = nuke.createNode('Write', 'file /tmp/Ls_nukeBLGresult.exr', False)
 	while(True):
 		# Block waiting for the fifo to be opened by Flame, render, then open the other fifo to signal back
 		open(frontreadyfifo, 'r').close()
@@ -30,12 +30,12 @@ import pybox_v1 as pybox
 class nukeBLG(pybox.BaseClass):
 	def initialize(self):
 		self.set_img_format('exr')
-		self.set_in_socket(0, 'Front', '/tmp/nukeBLGfront.exr')
+		self.set_in_socket(0, 'Front', '/tmp/Ls_nukeBLGfront.exr')
 		self.remove_in_socket(2)
-		self.set_out_socket(0, 'Result', '/tmp/nukeBLGresult.exr')
+		self.set_out_socket(0, 'Result', '/tmp/Ls_nukeBLGresult.exr')
 		self.remove_out_socket(1)
 		self.add_global_elements(pybox.create_file_browser('Open BLG...', '/', 'exr', '/'))
-		self.set_ui_pages(pybox.create_page('nukeBLG', 'Browser'))
+		self.set_ui_pages(pybox.create_page('Ls_nukeBLG', 'Browser'))
 		self.set_state_id('execute')
 
 		# Create the two FIFOs anew.  We use these to coordinate the Flame and Nuke processes
@@ -45,12 +45,12 @@ class nukeBLG(pybox.BaseClass):
 			os.mkfifo(f)
 
 		# Clean up from a possible previous instance
-		os.system('pkill -f /tmp/nukeBLG.py')
+		os.system('pkill -f /tmp/Ls_nukeBLG.py')
 		os.system('rm -f ' + blg)
 		# Make a copy of the current file because Flame moves it
-		os.system('cp -f ' + __file__ + ' /tmp/nukeBLG.py')
+		os.system('cp -f ' + __file__ + ' /tmp/Ls_nukeBLG.py')
 		# Start Nuke pointed to our copy of this file. Baselight requires USER and HOME to be set
-		os.system('USER=t HOME=/tmp ' + nukebin + ' -it /tmp/nukeBLG.py &')
+		os.system('USER=t HOME=/tmp ' + nukebin + ' -it /tmp/Ls_nukeBLG.py &')
 
 	def execute(self):
 		for change in self.get_ui_changes():
@@ -65,7 +65,7 @@ class nukeBLG(pybox.BaseClass):
 
 	def teardown(self):
 		# Kill our Nuke process
-		os.system('pkill -f /tmp/nukeBLG.py')
+		os.system('pkill -f /tmp/Ls_nukeBLG.py')
 
 # From here we're run by Flame on each frame
 p = nukeBLG(sys.argv[1])
